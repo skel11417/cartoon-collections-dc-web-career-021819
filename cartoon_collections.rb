4029ff6f89ae443ced4c2f44cccf4abbac019e45
Sean Kelly <shonkelli@gmail.com>
2019-02-04 23:58:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.find do |items|
-    
-  end
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |item|
+    cheese_types.include(item)
+  end
+
 end
